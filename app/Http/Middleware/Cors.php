5e700b49d8f535da352fea5d6bc5e159cceca2d8
Daniel Toader <daniel.toader@emag.ro>
2018-09-09 10:42:03 +0300

    cors with options?

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -14,15 +14,26 @@ class Cors
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure                 $next
+     *
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-        return $next($request)
+        //Intercepts OPTIONS requests
+        if ($request->isMethod('OPTIONS')) {
+            $response = response('', 200);
+        } else {
+            // Pass the request to the next middleware
+            $response = $next($request);
+        }
+
+        $response
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization');
+
+        return $response;
     }
 }
